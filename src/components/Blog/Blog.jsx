528c8c69497fe6bdafc3d9df931debc9eda0892e
Sabid Farhan <sfttanim@gmail.com>
2024-03-15 13:15:34 +0600

    added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,10 +1,38 @@
 import PropTypes from 'prop-types';
+import { FaRegBookmark } from "react-icons/fa";
 
-const Blog = ({blog}) => {
-    console.log(blog)
+const Blog = ({ blog }) => {
     return (
         <div>
-            
+            <img src={blog.cover_image} className='rounded-lg min-w-full my-10' alt={`Cover Picture of the title: ${blog.title}`} />
+
+            <div className="flex justify-between items-center my-5">
+                <div className="flex items-center gap-5">
+                    <img src={blog.author_image} className='h-16 rounded-full' alt="" />
+                    <div className="">
+                        <h2 className=''>{blog.author}</h2>
+                        <p className=''>{blog.posted_date}</p>
+                    </div>
+                </div>
+
+                <div className="flex gap-2 items-center">
+                    <h2>{blog.reading_time} min read</h2>
+                    <button><FaRegBookmark 
+                    className='h-10 text-red-500'
+                    onClick={}
+                    ></FaRegBookmark></button>
+                </div>
+            </div>
+
+            <h1 className="text-3xl font-bold my-5">{blog.title}</h1>
+
+            <div className="flex gap-4">
+                <p className=""># {blog.hashtags[0]}</p>
+                <p className=""># {blog.hashtags[1]}</p>
+            </div>
+
+            <a href="#">Mark as read</a>
+
         </div>
     );
 };
--- src/components/Header/Blogs/Bolgs.jsx
+++ src/components/Header/Blogs/Bolgs.jsx
@@ -13,7 +13,10 @@ const Bolgs = () => {
         <div className="md:w-2/3">
             <h1 className="text-3xl font-semibold ">All Blogs: {blogs.length}</h1>
             {
-                blogs.map(blog => <Blog key={blog.id} blog={blog}> </Blog>)
+                blogs.map(blog => <Blog 
+                    key={blog.id} 
+                    blog={blog}
+                    > </Blog>)
             }
         </div>
     );
