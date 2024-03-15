b4266a01536911e5b664552334a77708801221a7
Sabid Farhan <sfttanim@gmail.com>
2024-03-15 10:08:01 +0600

    added blogs and blog files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 
 import './App.css'
+import Bookmarks from './components/Bookmarks/Bookmarks'
 import Bolgs from './components/Header/Blogs/Bolgs'
 import Header from './components/Header/Header'
 
 function App() {
   return (
-    <>
-    <Header></Header>
-    <Bolgs></Bolgs>
-    </>
+    <section className='lg:container mx-auto'>
+
+      <Header></Header>
+
+      <div className='md:flex'>
+        <Bolgs></Bolgs>
+        <Bookmarks></Bookmarks>
+      </div>
+    </section>
   )
 }
 
--- src/components/Header/Blogs/Bolgs.jsx
+++ src/components/Header/Blogs/Bolgs.jsx
@@ -1,16 +1,20 @@
 import { useEffect, useState } from "react";
+import Blog from "../../Blog/Blog";
 
 const Bolgs = () => {
     const [blogs, setBlogs] = useState([]);
-    useEffect(()=>{
+    useEffect(() => {
         fetch('blogs.json')
-        .then(res=> res.json())
-        .then(data=> setBlogs(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setBlogs(data))
+    }, [])
 
     return (
-        <div>
-            
+        <div className="md:w-2/3">
+            <h1 className="text-3xl font-semibold ">All Blogs: {blogs.length}</h1>
+            {
+                blogs.map(blog => <Blog key={blog.id} blog={blog}> </Blog>)
+            }
         </div>
     );
 };
